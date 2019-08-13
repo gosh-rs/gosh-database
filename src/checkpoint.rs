9b23ea42fd58a9bba12927cf56ffdd7ebdcab4e6
Wenping Guo <ybyygu@gmail.com>
2019-08-13 10:56:24 +0800

    add Checkpoint trait; remove Checkpointing trait

--- src/checkpoint.rs
+++ src/checkpoint.rs
@@ -2,52 +2,47 @@ use crate::common::*;
 use crate::schema::*;
 use crate::*;
 
-pub trait Checkpointing {
-    /// Load checkpointed data associated with `k`.
-    fn load_checkpoint<T>(&self, k: &str) -> Result<T>
-    where
-        T: serde::de::DeserializeOwned;
-
-    /// Insert `value` into checkpoint database.
-    fn save_checkpoint<T: ?Sized>(&self, k: &str, value: &T) -> Result<()>
-    where
-        T: serde::Serialize;
-}
-
-impl Checkpointing for DbConnection {
-    /// Load checkpointed data associated with `k`.
-    fn load_checkpoint<T>(&self, k: &str) -> Result<T>
-    where
-        T: serde::de::DeserializeOwned,
-    {
+pub trait Checkpoint
+where
+    Self: Clone + serde::Serialize + serde::de::DeserializeOwned,
+{
+    /// Set a checkpoint
+    fn checkpoint(&self, db: &DbConnection, identifier: &str) -> Result<()> {
         use crate::schema::checkpoints::dsl::*;
 
-        let conn = self.get();
-        let encoded: Vec<u8> = checkpoints.filter(key.eq(k)).select(data).first(&*conn)?;
-        let d = bincode::deserialize(&encoded[..]).unwrap();
-        Ok(d)
-    }
-
-    /// Insert `value` into checkpoint database.
-    fn save_checkpoint<T: ?Sized>(&self, k: &str, value: &T) -> Result<()>
-    where
-        T: serde::Serialize,
-    {
-        use crate::schema::checkpoints::dsl::*;
-
-        let conn = self.get();
-        let row = (key.eq(k), data.eq({ bincode::serialize(value).unwrap() }));
+        let conn = db.get();
+        let row = (
+            key.eq(identifier),
+            data.eq({ bincode::serialize(&self).unwrap() }),
+        );
         diesel::insert_into(checkpoints)
             .values(&row)
             .execute(&*conn)
             .with_context(|_| {
                 format!(
                     "Failed to save checkpoint\n chk key: {}\n db source: {}",
-                    k,
-                    self.database_url()
+                    identifier,
+                    db.database_url()
                 )
             })?;
 
         Ok(())
     }
+
+    /// Load from checkpointed state.
+    fn restore_from_checkpoint(&mut self, db: &DbConnection, identifier: &str) -> Result<()> {
+        use crate::schema::checkpoints::dsl::*;
+
+        let conn = db.get();
+        let encoded: Vec<u8> = checkpoints
+            .filter(key.eq(identifier))
+            .select(data)
+            .first(&*conn)?;
+        let x = bincode::deserialize(&encoded[..]).unwrap();
+        self.clone_from(&x);
+        Ok(())
+    }
 }
+
+impl Checkpoint for gosh_models::ModelProperties {}
+impl Checkpoint for gchemol::Molecule {}
